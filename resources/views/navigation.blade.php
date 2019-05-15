5ce437f4355ecb4b469fc205c9efa68345c7760a
Mike Bronner <mike@genealabs.com>
2019-05-15 09:10:10 -0700

    Update Nova menu icon

--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -1,11 +1,15 @@
 <router-link tag="h3" :to="{name: 'tenants'}" class="cursor-pointer flex items-center font-normal dim text-white mb-6 text-base no-underline">
-    <i class="sidebar-icon fab fa-fw fa-btn fa-lg fa-fort-awesome mr-1"></i>
+    <span class="text-grey">
+        <i class="sidebar-icon fab fa-fw fa-btn fa-lg fa-fort-awesome mr-1"></i>
+    </span>
     <span class="sidebar-label">
         Tenants
     </span>
 </router-link>
 <router-link tag="h3" :to="{name: 'tenant-settings'}" class="cursor-pointer flex items-center font-normal dim text-white mb-6 text-base no-underline">
-    <i class="sidebar-icon fas fa-fw fa-btn fa-lg fa-tools mr-1"></i>
+    <span class="text-grey">
+        <i class="sidebar-icon fas fa-fw fa-btn fa-lg fa-tools mr-1"></i>
+    </span>
     <span class="sidebar-label">
         Site Settings
     </span>
