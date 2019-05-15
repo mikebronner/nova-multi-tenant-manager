66514d81f5abf37041c0aa84141a4d670eabe720
Mike Bronner <mike@genealabs.com>
2019-05-15 14:33:46 -0700

    Tweak Nova menu icons

--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -1,14 +1,14 @@
 <router-link tag="h3" :to="{name: 'tenants'}" class="cursor-pointer flex items-center font-normal dim text-white mb-6 text-base no-underline">
-    <span class="text-grey">
-        <i class="sidebar-icon fab fa-fw fa-btn fa-lg fa-fort-awesome mr-1"></i>
+    <span class="text-grey" style="font-size: 20px;">
+        <i class="sidebar-icon fab fa-fort-awesome mr-3"></i>
     </span>
     <span class="sidebar-label">
         Tenants
     </span>
 </router-link>
 <router-link tag="h3" :to="{name: 'tenant-settings'}" class="cursor-pointer flex items-center font-normal dim text-white mb-6 text-base no-underline">
-    <span class="text-grey">
-        <i class="sidebar-icon fas fa-fw fa-btn fa-lg fa-tools mr-1"></i>
+    <span class="text-grey" style="font-size: 20px;">
+        <i class="sidebar-icon fas fa-tools mr-3"></i>
     </span>
     <span class="sidebar-label">
         Site Settings
